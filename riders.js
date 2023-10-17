da95e23c891638135162f804a8eb31ee761dc69b
Mati <matiasferreras33@gmail.com>
2023-10-18 01:13:32 +0200

    riders cards + footer + other updates

--- riders.js
+++ riders.js
@@ -7,26 +7,51 @@ class Rider{
         this._country = country;
         this._engine = engine;
         this._frame = frame;
+        this._facebook = facebook;
+        this._birthdate = birthdate;
+        
     } 
     getDetails() {
         return `Name: ${this._name}, Number: ${this._number}, Favorite Rider: ${this._favrider}, Favorite Track: ${this._favtrack}, Country: ${this._country}, Engine: ${this._engine}, Frame: ${this._frame}`;
     }
+    getName(){
+        return this._name;
+    }
 };
 
+
 const Rider1 = new Rider("Waldek Kroll",31,"Zmarzlik","Gdańsk","Poland","GM offset", "Stuha","waldek.kroll");
-const Rider2 = new Rider("Robert Mechlik",43,"Gollob","Gdańsk","Poland","GM standard", "Jawa","robert.mechlik");
+const Rider2 = new Rider("Robert Mechlik",43,"Gollob","Gdańsk","Poland","GM standard", "Jawa","robert.mechlik", "19.19.1990");
 const Rider3 = new Rider("Łukasz Pierkoś",20,"Gollob","Gdańsk","Poland","GM standard", "Jawa");
-const Rider4 = new Rider("Dariusz Dymek",175,"Gollob","Gdańsk","Poland","GM standard", "Jawa","dariusz.dymek.777");
+const Rider4 = new Rider("Dariusz Dymek",175,"Gollob","Gdańsk","Poland","GM standard", "Jawa","dariusz.dymek.777","01.01.1975");
+const Rider5 = new Rider("Wojciech Stachacz",15,"Gollob","Gdańsk","Poland","GM standard", "Jawa","dariusz.dymek.777");
+const Rider6 = new Rider("Dawid Dymek",175,"Gollob","Gdańsk","Poland","GM standard", "Jawa","dariusz.dymek.777");
 const Trener = new Rider("Eryk Jóżwiak","","","Gdańsk","Poland","","","","eryk.jozwiak.1")
 console.log(Rider1.getDetails())
+console.log(Rider1.getName())
+
 
 let Waldekdetails = Rider1.getDetails();
 document.getElementById('Waldekdetails').innerHTML = Waldekdetails;
 
+let Waldekname = Rider1.getName();
+document.getElementById('Waldekname').innerHTML = Waldekname;
+
 let Robertdetails = Rider2.getDetails();
 document.getElementById('Robertdetails').innerHTML = Robertdetails;
 
+let Lukaszdetails = Rider3.getDetails();
+document.getElementById('Lukaszdetails').innerHTML = Lukaszdetails;
+
 let Dariuszdetails = Rider4.getDetails();
 document.getElementById('Dariuszdetails').innerHTML = Dariuszdetails;
+Rider4.getName();
+document.getElementById('Dariuszname').innerHTML = Dariuszname;
+
+let Wojciechdetails = Rider5.getDetails();
+document.getElementById('Wojciechdetails').innerHTML = Wojciechdetails;
+
+let Dawiddetails = Rider6.getDetails();
+document.getElementById('Dawiddetails').innerHTML = Dawiddetails;
 
 
---
+++ riders_cards.js
@@ -0,0 +1,87 @@
+let originalSrc;
+
+document.getElementById('Waldek_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/353068864_274962094914217_7919074638914782578_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=O386uv8lKSoAX-4UyKs&_nc_ht=scontent-waw1-1.xx&oh=00_AfD1O8IqHMV9QAH7eaHiu1zD89zOK3S0bS07E_omG2Sj7g&oe=652ED9D8';
+    document.getElementById('Table_Waldek').style.display = 'block';
+});
+document.getElementById('Waldek_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Waldek').style.display = 'none';
+
+});
+// ----------------------------------------------------------------------------------------------------------------------------------
+document.getElementById('Robert_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/355684926_283165254093901_5001262912995261749_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=5f2048&_nc_ohc=ykXBxWgbV7sAX-FzMEc&_nc_ht=scontent-waw1-1.xx&oh=00_AfDQg2CqsuwPxXFaDre5OEyldFslNmorqQUEiPDyRZZ-ww&oe=652EA2C1';
+    document.getElementById('Table_Robert').style.display = 'block';
+});
+document.getElementById('Robert_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Robert').style.display = 'none';
+
+});
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+
+document.getElementById('Dariusz_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/349004762_274962511580842_6274746838223589923_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=CNb2_BndHycAX9kPKrM&_nc_ht=scontent-waw1-1.xx&oh=00_AfAbH8XXDELy5Pay1WALijC1Nyzet2s0H6ZXyvug7q67HQ&oe=652D7E3F';
+    document.getElementById('Table_Dariusz').style.display = 'block';
+});
+document.getElementById('Dariusz_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Dariusz').style.display = 'none';
+
+});
+
+// ----------------------------------------------------------------------------------------------------------------------------------
+document.getElementById('Lukasz_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/367414585_775939411199760_1973001066962658240_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=liwfXdF-aegAX8kzywK&_nc_ht=scontent-waw1-1.xx&oh=00_AfBgvm1lxmR6alBu3Xj9iWj-z60nyfDtTP9cdeXtpmEjPw&oe=652E11C9';
+    document.getElementById('Table_Lukasz').style.display = 'block';
+});
+document.getElementById('Lukasz_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Lukasz').style.display = 'none';
+
+});
+
+//------------------------------------------------------------------------------------------------------------------------------------
+
+document.getElementById('Dawid_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/381677838_6732876480138567_2872472436998632576_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4ZFvmBXV5PEAX9bbLfc&_nc_ht=scontent-waw1-1.xx&oh=00_AfADNML_PUqh-4csHNJHLcm0gWrSd-nG0yc4na1INS1yBA&oe=652D4DDB';
+    document.getElementById('Table_Dawid').style.display = 'block';
+});
+document.getElementById('Dawid_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Dawid').style.display = 'none';
+
+});
+
+//-----------------------------------------------------------------------------------------------------------
+
+document.getElementById('Wojciech_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/355515008_283164930760600_8767168663637689363_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kHtwR8zlVpMAX8_je3f&_nc_ht=scontent-waw1-1.xx&oh=00_AfCpQ_rAY1CRRC-evoPfVyrAMTTTzNiCskWASTUQL1hXNw&oe=65338220';
+    document.getElementById('Table_Wojciech').style.display = 'block';
+});
+document.getElementById('Wojciech_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Wojciech').style.display = 'none';
+
+});
+
+
+//----------------------------------------------------------------------------------------------------------------
+document.getElementById('Eryk_id').addEventListener('mouseover', function() {
+    originalSrc = this.src;
+    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t31.18172-8/1965605_913244475357308_7724378376479458163_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=4dc865&_nc_ohc=_fY1abgCHagAX_y3PZp&_nc_ht=scontent-waw1-1.xx&oh=00_AfAl0I4w7VOSJZr2ym0Guxo3LwvD3xqyW7hBwhEpd2qxIA&oe=65568341';
+    document.getElementById('Table_Eryk').style.display = 'block';
+});
+document.getElementById('Eryk_id').addEventListener('mouseout', function() {
+    this.src = originalSrc;
+    document.getElementById('Table_Eryk').style.display = 'none';
+
+});
