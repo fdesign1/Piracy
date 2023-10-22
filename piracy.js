529dd8a9045fe2546a8ce2402ca952da5d29e8b6
Mati <matiasferreras33@gmail.com>
2023-10-23 01:47:26 +0200

    carousel transition effect

--- piracy.js
+++ piracy.js
@@ -1,15 +1,12 @@
 const myCarouselElement = document.querySelector('.carousel-item')
 
+
 const carousel = new bootstrap.Carousel(myCarouselElement, {
-  interval: 500,
-  touch: false
-})
+  interval: 10,
 
-
-const myCarousel = document.getElementById('.carousel-item')
-
-myCarousel.addEventListener('slide.bs.carousel', event => {
-  
 });
+
+
+
 
 
