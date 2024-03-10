48a5e26e6418173e71b5f00c74cce2a0bc3afcfc
Mati <67325501+fdesign1@users.noreply.github.com>
2024-03-10 15:38:54 +0000

    checking api weather

--- piracy.js
+++ piracy.js
@@ -7,6 +7,34 @@ const carousel = new bootstrap.Carousel(myCarouselElement, {
 });
 
 
+const apiKey = 'PqBGHH6pfZu6p7JfG518FeUuhqyMj8hD';
 
+const location = 'Gdańsk';
+
+const url = `http://dataservice.accuweather.com/currentconditions/v1/${location}?apikey=${apiKey}`;
+
+
+async function fetchWeather() {
+  try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      // Manipular los datos de respuesta
+      const weatherInfo = data[0];
+      const temperature = weatherInfo.Temperature.Metric.Value;
+      const weatherText = weatherInfo.WeatherText;
+
+      // Mostrar la información en la página web
+      document.getElementById('weather-info').innerHTML = `
+          <p>Temperature: ${temperature}°C</p>
+          <p>Weather: ${weatherText}</p>
+      `;
+  } catch (error) {
+      console.error('Error fetching data:', error);
+      document.getElementById('weather-info').innerHTML = '<p>Error fetching weather data</p>';
+  }
+}
+
+fetchWeather();
 
 
