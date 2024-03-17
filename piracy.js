a6adaa91b091cd3a7d9d071462a0cf93c794c968
Mati <matiasferreras33@gmail.com>
2024-03-17 23:44:47 +0100

    added clima icons

--- piracy.js
+++ piracy.js
@@ -1,12 +1,55 @@
-const myCarouselElement = document.querySelector('.carousel-item')
+/*const myCarouselElement = document.querySelector('.carousel-item')
 
 
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 10,
 
-});
+});*/
 
 
+const apiKey = 'PqBGHH6pfZu6p7JfG518FeUuhqyMj8hD';
+
+const cityName = 'Gdańsk'; // Nombre de la ciudad que queremos buscar
+const countryCode = 'PL'; // Código del país (en este caso, Polonia)
+
+// URL para la API de Autocompletado de Localización de AccuWeather
+const autoCompleteUrl = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apiKey}&q=${cityName}&country=${countryCode}`;
+
+async function fetchWeather() {
+    try {
+        // Realizar la solicitud para obtener el código de ubicación de Varsovia
+        const autoCompleteResponse = await fetch(autoCompleteUrl);
+        const locationData = await autoCompleteResponse.json();
+
+        // Obtener el código de ubicación de Varsovia
+        const cityData = locationData[0];
+        const locationKey = cityData.Key;
+
+        // Utilizar el código de ubicación para obtener el pronóstico del tiempo para Varsovia
+        const weatherUrl = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
+
+        // Realizar la solicitud para obtener el pronóstico del tiempo
+        const weatherResponse = await fetch(weatherUrl);
+        const weatherData = await weatherResponse.json();
+
+        // Manipular los datos de respuesta del pronóstico del tiempo
+        const temperature = weatherData[0].Temperature.Metric.Value;
+        const weatherText = weatherData[0].WeatherText;
+
+        // Mostrar la información en la página web 1
+        document.getElementById('weather-info').innerHTML = `
+            <p> Gdańsk: ${temperature}°C,${weatherText}</p>
+        `;
+
+
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        document.getElementById('weather-info').innerHTML = '<p>Error fetching data</p>';
+    }
+}
+
+fetchWeather();
 
 
 
--- riders_cards.js
+++ riders_cards.js
@@ -2,7 +2,7 @@ let originalSrc;
 
 document.getElementById('Waldek_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/353456266_274962001580893_3467884242510310051_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=bkI7vzcM9X4AX_xy6pa&_nc_ht=scontent-waw1-1.xx&oh=00_AfDyhQPU5zfxMpHZYDujKuGT3hvo9pP4PoibArqFlJ95ow&oe=6536B539'
+    this.src = 'https://previews.dropbox.com/p/thumb/ACMBUPYHJfKly5e_hF2XIjldBaigGlRaCLGplC-wJgl2KACkyP8H5sd6azuy3NILeqMgE5c7AXUU6NBwHIwhlkZe_ViThJS09HnEGYZ7WlbD36qDMbvPtJB8JW2rLfDr7ssFkEkM53lSigeL83fSqKQzybNVtmdgCuw22Bn4FD288ReSh3aaV5SS38_9nfM7_yxovEsjl0RFKS3TmWHtB9d0KIPzPHrdCHd16edGp5ND-jy5_p7upm5d04pcKtW5p0IQZuH_tKZX_X7qxfenphQp1Oqav9wZd88UPNCTJJbep-NH18W9Lq4hSrpU-JYJbr4vY0iUBxUlH7qa0iFz21B0/p.jpeg';
     document.getElementById('Table_Waldek').style.display = 'block';
 });
 document.getElementById('Waldek_id').addEventListener('mouseout', function() {
@@ -13,7 +13,7 @@ document.getElementById('Waldek_id').addEventListener('mouseout', function() {
 // ----------------------------------------------------------------------------------------------------------------------------------
 document.getElementById('Robert_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/353460995_274962191580874_8097842282206814821_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=EwTDYopociEAX_hy4fH&_nc_ht=scontent-waw1-1.xx&oh=00_AfAz0-BwV3IgxboeHdSz9BcP4meZZM663PUM3ZF_bPbeYg&oe=6537AE4E';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACPBVaQrSieM_FrudQ_M4IVa36ONGJuDL7vP5JP4OYunKmy5hBGUrsDk-Qus0CLocDbIwr3K9OJVZJT4iped3FFPIyjehN8CPaCPpYm1ItX0jwupi1zvt-5zV9YuLz2N25GqCFBcZFSRBA7vaFwsBJTwAKv_Bk1PWYGvpObZbgELbClZf7MG0HtWnXRtxMXB41_JL0Z9jG3zM0trJckuV5Os-0ARuIOrIfjQStdgPC4Pjw/p.jpeg';
     document.getElementById('Table_Robert').style.display = 'block';
 });
 document.getElementById('Robert_id').addEventListener('mouseout', function() {
@@ -26,7 +26,7 @@ document.getElementById('Robert_id').addEventListener('mouseout', function() {
 
 document.getElementById('Dariusz_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/349004762_274962511580842_6274746838223589923_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=GV-EvEcYeGkAX8T9cnt&_nc_ht=scontent-waw1-1.xx&oh=00_AfAYwxNK7DwBlN4FT6u4Ztg-H9EzSESjzGZzXoa97oIbyg&oe=6537617F';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACPKq8PaHPEItxvz83ugwyJOnjpeE57951gs4uwXcGfVxXv67xKeiRwSkZ_H0VBTz77us14u2uqX2iQSSIF2d5PGkIgZNMo21I2EKIZhsMQiWZjbTYGqPJWYmeBb2QuUSU73krA8zvjUxYycNbLmKUygApT31rn6zDJMoVGgBBkeLg6FW3XIt__OoOiVKY43OuDU8Y9zPpr1L9bouUddwLMOJMJaP69Y9JH3RRDge70DER50Gimlmr3TSGS-c4LKP6blgJgnRcUAYyTKcwx0nMDez2fFzJKyGm4zKUnBx-OLkMoF2au1n70VOJvqCfpDB_NvuUz3Vdt0sHydQsX8zROW/p.jpeg';
     document.getElementById('Table_Dariusz').style.display = 'block';
 });
 document.getElementById('Dariusz_id').addEventListener('mouseout', function() {
@@ -37,7 +37,7 @@ document.getElementById('Dariusz_id').addEventListener('mouseout', function() {
 // ----------------------------------------------------------------------------------------------------------------------------------
 document.getElementById('Lukasz_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/352322629_279136174496809_3172107967548987556_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Su_D6iTdn00AX9EZm3I&_nc_ht=scontent-waw1-1.xx&oh=00_AfAjt9-qU9fQaWlIQpsiXQVE099dUM-inupgjFJ0n0pBWA&oe=6536BF92';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACMdwwWbGnan33voKrVjwge4S1SrLvbC4_zBa2vmvKZc6PrXsEur2PMC0UYI_G-lD3HVXxF6wuAFnDAW7zTzgbAHSO7HnNt3iqEOaseSZfKuyKOhSw8jt1Oz8qxvd3laeRhqjQDALKSGTAHLglrnGeCtwRSMu6B9Tw4B7qEh7rMden8pODvyeOr2tx4xSa3QeUg5yoZbMgx6JO_5S-vJ_tKcikbkuyMpiEzySR9QRCcfOEfwmfiJdANIxt0lDgdu1bzjbyPyjeqXMLnIeUZPJwiLz0I7sQXgpc7RMZ47LslaFOA71GbHtZgMN-8q1h08W1U2zRKW2w_pCjTQnRcGzjXZ/p.jpeg';
     document.getElementById('Table_Lukasz').style.display = 'block';
     
 });
@@ -51,7 +51,7 @@ document.getElementById('Lukasz_id').addEventListener('mouseout', function() {
 
 document.getElementById('Dawid_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/355308340_283165104093916_8534133493789141167_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=nlsXvUaSxmcAX83EPAq&_nc_ht=scontent-waw1-1.xx&oh=00_AfBNpO9AXqoEBIAx8zPuUmDrl1YAF1oijS6kka5F6b_4hA&oe=65387731';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACMuVPFjyvhQQTo41whlyM8NFxk0o9GwxcLf3uux67O0h3Z5_PTpprj93h6gx-tC7jFTH38sjVdaoAcBPdflSM2j1ACFDwrs8yayKLr6hrULtpo0_8K8w0krEMYHAd3zNjVVwzzqzSAtclMJ33NJ8upooC0kkYGujdnLNqih0lB16WQfmfqAh7UCyxQZ8sjaixpvXCsQdFP4I7czLeQvBG1vFAKOnvFulX8p6WcGJWUN1xUi6BlG9kSX7uK-_4RZTJMfUg93XBGdWCIF1mQBv_xdyyy_xTVu_2sPy21dywmq-ojCh2sbAfT8k1plcvhEo2qBUNOoKy9PGhIe9Hm0lWBd/p.jpeg';
     document.getElementById('Table_Dawid').style.display = 'block';
 });
 document.getElementById('Dawid_id').addEventListener('mouseout', function() {
@@ -64,7 +64,7 @@ document.getElementById('Dawid_id').addEventListener('mouseout', function() {
 
 document.getElementById('Kuba_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t39.30808-6/355515008_283164930760600_8767168663637689363_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=kHtwR8zlVpMAX8_je3f&_nc_ht=scontent-waw1-1.xx&oh=00_AfCpQ_rAY1CRRC-evoPfVyrAMTTTzNiCskWASTUQL1hXNw&oe=65338220';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACP6qbT2l4qckjwoCZIk3Bw1cN9XVAOdiOZEV23dhqlGpNTDFzO7qv-LM9pKpvCyTgafwPghKzn_-Z6nMLFZFRufWOqV0kFv1d6RkqKPsBxy6Sll8kjRlkAytXzExg1L8NkEQb29JZY9LM-9u1sE1_GVhzdGQtdvYei8mhh2GhKJhVaEN9syZJNMM1CweFUsbS9X0o_PFtbl870Gz5d0b3wC1f7l01WnsOTRdUZpwqYZCqPHXN1wlPfjR2mDzobcNlk79MrzFbM5CI8OcMSGHEVlK732kdsJH5z6Ofw_e3Ji8QByHLZoXm0nDbuIiy4o5OyT_XbDyj5CtvZF51ri0SDA/p.jpeg';
     document.getElementById('Table_Kuba').style.display = 'block';
 });
 document.getElementById('Kuba_id').addEventListener('mouseout', function() {
@@ -77,7 +77,7 @@ document.getElementById('Kuba_id').addEventListener('mouseout', function() {
 //----------------------------------------------------------------------------------------------------------------
 document.getElementById('Eryk_id').addEventListener('mouseover', function() {
     originalSrc = this.src;
-    this.src = 'https://scontent-waw1-1.xx.fbcdn.net/v/t31.18172-8/1965605_913244475357308_7724378376479458163_o.jpg?_nc_cat=101&ccb=1-7&_nc_sid=4dc865&_nc_ohc=_fY1abgCHagAX_y3PZp&_nc_ht=scontent-waw1-1.xx&oh=00_AfAl0I4w7VOSJZr2ym0Guxo3LwvD3xqyW7hBwhEpd2qxIA&oe=65568341';
+    this.src = 'https://previews.dropbox.com/p/thumb/ACPsJh7NQISWfGV4q3spCCIj93ylU9eFOsS7WlzBnneY7dGeGJro9lqz_U9FbDzX0ZFicdWBwXLWFTDA5vYRcBfgX9dWsm1HVjcZ9NKx_rhB9vH_TUU6hH-VC7KJiFPVq0mgyfbdLmdm8wKlCKReLy_Znbwh4CyrNVmUB7LKRIm4CWjJUnsaIXsKyJ5F3FDPsFJ8dMOIu35gJ57Acy3Axrm9kzlJ1iqSJxj8u05qQUwSfb07qRQ8TfmQAvSoecwZE85vB2iKXUQ-LpPKx7b1_7ODTlcYwx_dWYYufuOy7Cf0xg4dkRjEJxSDDtYR6x4ZZsjpOCL6bmQU2Y3JOC6xfIGj/p.jpeg';
     document.getElementById('Table_Eryk').style.display = 'block';
 });
 document.getElementById('Eryk_id').addEventListener('mouseout', function() {
